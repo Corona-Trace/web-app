ce6eb800469a20fff15eed246f2c1d9352eb1a75
devansh2001 <dponda3@gatech.edu>
2020-03-28 21:56:27 -0400

    Added opening Google Takeout dashboard in new tab

--- src/upload-location-history/GoogleTakeout.js
+++ src/upload-location-history/GoogleTakeout.js
@@ -5,7 +5,25 @@ import HowToGuide from "./HowToGuide";
 class GoogleTakeout extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            userEmail: ''
+        }
     }
+
+    handleAccountDownloadButtonClick = (event) => {
+        const form = event.currentTarget;
+        if (form.checkValidity()) {
+            var url = 'https://accounts.google.com/ServiceLogin?service=mail&passive=true&Email=' + this.state.userEmail + '&continue=https://takeout.google.com/settings/takeout/custom/location_history?hl=en&gl=US&expflags';
+            window.open(url);
+        }
+
+    };
+
+    handleEmailChange = (event) => {
+        this.setState({
+            userEmail: event.target.value
+        });
+    };
 
     render() {
         return (
@@ -16,10 +34,10 @@ class GoogleTakeout extends Component {
                     </Row>
                     <Row>
                         <Col xs={4}>
-                            <Form>
+                            <Form onSubmit={this.handleAccountDownloadButtonClick} id={'email-form'}>
                                 <Form.Group controlId="formBasicEmail">
                                     <h3>Email address</h3>
-                                    <Form.Control type="email" placeholder="your_email@gmail.com" />
+                                    <Form.Control type="email" placeholder="your@email.com" onChange={this.handleEmailChange}/>
                                     <Form.Text className="text-muted">
                                         We'll never share your email with anyone else.
                                     </Form.Text>
